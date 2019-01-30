2f88faf084fcad95178e8664b53dd90ba95752e0
Adam Hosman <github@adamsinbox.com>
2019-01-30 22:17:39 +0100

    Create table `users`

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,18 @@
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    locality VARCHAR(100),
+    country VARCHAR(100),
+    password_hash CHAR(60) BINARY NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created DATETIME NOT NULL DEFAULT NOW(),
+    last_updated DATETIME DEFAULT NULL ON UPDATE NOW(),
+    admin_rights INT NOT NULL DEFAULT 0
+);
+
+
 CREATE TABLE questions (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     view_count INT NOT NULL DEFAULT 0,
